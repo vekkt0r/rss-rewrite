71dd2ec09321f47359739169f200ec71a8af0192
vekkt0r <adreg@megalan.org>
2022-11-06 15:10:25 +0100

    Update filter-rss.sh

--- filter-rss.sh
+++ filter-rss.sh
@@ -2,4 +2,4 @@
 
 mkdir public
 # Duplicate the Comments link with darkhn variant
-curl http://hnapp.com/rss?q=score%3E85 | sed 's/\(&lt;a href.*\)news.ycombinator.com\(.*a&gt\);/&\&lt;br\&gt;\1darkhn.herokuapp.com\2/' > public/hn.rss
+curl http://hnapp.com/rss?q=score%3E85 | sed 's/\(&lt;a href.*\)news.ycombinator.com\(.*a&gt;\)/&\&lt;br\/\&gt;\1darkhn.herokuapp.com\2/' > public/hn.rss
