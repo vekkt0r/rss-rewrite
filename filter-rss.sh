f9350ade6f241d7864dc656cdc53d4229481ed7f
vekkt0r <adreg@megalan.org>
2022-11-06 15:05:10 +0100

    Create filter-rss.sh

---
+++ filter-rss.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir public
+# Duplicate the Comments link with darkhn variant
+curl http://hnapp.com/rss?q=score%3E85 | sed 's/\(&lt;a href.*\)news.ycombinator.com\(.*a&gt\);/&\&lt;br\&gt;\1darkhn.herokuapp.com\2/' > public/hn.rss
